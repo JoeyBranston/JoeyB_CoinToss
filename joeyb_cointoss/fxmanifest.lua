f2d4c06b2225aa4cc25ddbfde77ca15a7d3b88d0
Joey Branston <124396058+JoeyBranston@users.noreply.github.com>
2025-02-16 14:04:39 -0330

    Create fxmanifest.lua

---
+++ joeyb_cointoss/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Joey Branston'
+description 'A simple script that allows players to do a coin toss using an item'
+version '1.0.0'
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+dependencies {
+    'qb-core',
+    'interact-sound' 
+}
